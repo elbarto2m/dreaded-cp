624fa7434f1034a8b243e9c090ee0c60f8ed53d2
Elbarto <113073680+elbarto2m@users.noreply.github.com>
2023-04-28 02:59:43 +0100

    Update index.ts

--- src/Types/index.ts
+++ src/Types/index.ts
@@ -1,8 +1,9 @@
-import Baileys, { GroupMetadata, ParticipantAction } from '@adiwajshing/baileys'
+import Baileys, { ParticipantAction, GroupMetadata } from '@adiwajshing/baileys'
 
 export * from './Config'
 export * from './Command'
 export * from './Message'
+export * from './Pokemon'
 
 export interface IContact {
     jid: string
@@ -12,6 +13,10 @@ export interface IContact {
 
 export interface ISender extends IContact {
     isAdmin: boolean
+}
+
+export interface IGroup extends GroupMetadata {
+    admins?: string[]
 }
 
 export interface ICall {
@@ -29,36 +34,71 @@ export interface IEvent {
     action: ParticipantAction
 }
 
-export interface YT_Search {
-    type: string
-    videoId: string
-    url: string
-    title: string
-    description: string
-    image: string
-    thumbnail: string
-    seconds: number
-    timestamp: string
-    duration: {
-        seconds: number
-        timestamp: string
-    }
-    ago: string
-    views: number
-    author: {
-        name: string
-        url: string
-    }
+export interface WaifuResponse {
+    id: string
+    slug: string
+    name: string
+    original_name?: string
+    romaji_name?: string
+    display_picture: string
+    description?: string
+    weight?: string | number
+    height?: string | number
+    bust?: string | number
+    hip?: string | number
+    waist?: string | number
+    blood_type?: string
+    origin?: string
+    age?: number
+    birthday_month?: string
+    birthday_day?: number
+    birthday_year?: string | number
+    likes?: number
+    trash?: number
+    popularity_rank?: number
+    like_rank?: number
+    trash_rank?: number
+    husbando: boolean
+    nsfw: boolean
+    creator?: { id: number; name: string }
+    tags?: string[]
+    url?: string
+    appearances?: series[]
+    series?: series
 }
 
-export enum GroupFeatures {
-    'events' = 'By enabling this feature, the bot will welcome new members, gives farewell to the members who left the group or were removed and reacts when a member is promoted or demoted',
-    'mods' = "By enabling this feature, it enables the bot to remove the member (except for admins) which sent an invite link of other groups. This will work if and only if the bot's an admin",
-    'nsfw' = 'By enabling this feature, it enables the bot to send *NSFW* contents'
+export interface series {
+    name: string
+    original_name?: string
+    romaji_name?: string
+    description?: string
+    slug?: string
+    airing_start?: string
+    airing_end?: string
+    episode_count?: string
+    release?: string
+    display_picture?: string
+    studio?: string
 }
 
-export interface IGroup extends GroupMetadata {
-    admins?: string[]
+export interface IPokemonChainResponse {
+    species: IPokemonSpecies
+    evolves_to: IPokemonEvolvesTo[]
 }
+
+export interface IPokemonSpecies {
+    name: string
+}
+
+export interface IPokemonEvolvesTo {
+    species: IPokemonSpecies
+    evolves_to: {
+        species: IPokemonSpecies
+    }[]
+}
+
+export type TCardsTier = '1' | '2' | '3' | '4' | '5' | '6' | 'S'
+
+export type TGroupFeature = 'news' | 'events' | 'nsfw' | 'cards' | 'wild' | 'mods'
 
 export type client = ReturnType<typeof Baileys>
